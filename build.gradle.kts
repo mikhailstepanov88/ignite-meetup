4eab500fe52949f3fba0c70483180187ca9f1bf7
Michael Stepanov <stepanovmixail@gmail.com>
2018-08-29 20:34:57 +0300

    Add scripts and gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.4.RELEASE")
+    }
+}
+
+allprojects {
+    repositories {
+        jcenter()
+    }
+}
